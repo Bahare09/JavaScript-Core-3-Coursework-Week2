80fd4494fd46de699fe4a22c638f89020c7c7b12
Bahare <itxbb09@gmail.com>
2023-02-03 20:00:33 +0000

    dog-photo-gallery Done

---
+++ challenges/dog-photo-gallery/script.js
@@ -0,0 +1,16 @@
+const clickButton = document.getElementById("clickBtn");
+clickButton.addEventListener("click", () => {
+  return fetch("https://dog.ceo/api/breeds/image/random")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (image) {
+      let list = document.getElementById("imageList");
+      let imgLi = document.createElement("li");
+      list.appendChild(imgLi);
+      let img = document.createElement("img");
+      imgLi.appendChild(img);
+      img.src = image.message;
+    })
+    .catch((error) => console.log(error));
+});
