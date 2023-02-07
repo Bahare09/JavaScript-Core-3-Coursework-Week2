f13dc2ab16e4aff547edb4273c5102e584fc2935
Bahare <itxbb09@gmail.com>
2023-02-07 09:44:15 +0000

    programmer-humour Done

--- challenges/programmer-humour/script.js
+++ challenges/programmer-humour/script.js
@@ -1,12 +1,11 @@
-
-
 async function programmer() {
- try{ const data = await fetch("https://xkcd.now.sh/?comic=latest");
-  const value = await data.json();
-  console.log(value)
-  document.getElementById("image").src = value.img;}
-  catch(err){
-    console.log("Error")
+  try {
+    const data = await fetch("https://xkcd.now.sh/?comic=latest");
+    const value = await data.json();
+    console.log(value);
+    document.getElementById("image").src = value.img;
+  } catch (err) {
+    console.log("Error");
   }
 }
 programmer();
